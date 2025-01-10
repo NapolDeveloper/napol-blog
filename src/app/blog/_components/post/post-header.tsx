88f9af7a034632860c218ae8c3b96215df21926f
NapolDeveloper <loma1016@gmail.com>
2025-01-10 10:28:38 +0900

    feat: 포스트 상단 헤더 영역 구현

---
+++ src/app/blog/_components/post/post-header.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import { CalendarDays } from 'lucide-react';
+
+import { Link as LinkIcon } from 'lucide-react';
+
+import { Text } from '@/components/ui/text';
+import { Post } from '@/models/post';
+import IconButton from '@/components/shared/icon-button';
+import { Button } from '@/components/ui/button';
+
+interface Props {
+  post: Post;
+}
+
+export default function PostHeader({ post }: Props) {
+  return (
+    <header className="mb-[50px] text-center">
+      <Text as="h1" size={'4xl'} weight={'extrabold'} className="mb-5">
+        {post.title}
+      </Text>
+
+      {/* 카테고리 */}
+      <Link
+        href={`/blog/${post.category}`}
+        className="font-semibold text-blue-600 no-underline underline-offset-4 hover:underline"
+      >
+        {post.category}
+      </Link>
+
+      {/* 작성일 */}
+      <div className="mt-4 flex items-center justify-center gap-2">
+        <CalendarDays className="w-3.5 text-muted-foreground" />
+        <Text size="sm" color={'muted'}>
+          {post.dateString}
+        </Text>
+      </div>
+
+      <hr className="mt-5" />
+    </header>
+  );
+}
