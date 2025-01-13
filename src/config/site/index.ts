868c388f4a7bc3dc2a6339ddbf54133b619e3410
NapolDeveloper <loma1016@gmail.com>
2025-01-13 11:25:13 +0900

    feat: site config 파일 작성

---
+++ src/config/site/index.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: '개발블로그',
+  url: 'http://localhost:3000',
+  ogImage: '추후 추가 필요',
+  description: '프론트 개발자의 개발 블로그',
+};
+
+export type SiteConfig = typeof siteConfig;
+
+export const META_THEME_COLORS = {
+  light: '#ffffff',
+  dark: '#09090b',
+};
