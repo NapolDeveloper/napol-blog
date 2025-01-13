c044b0a303ac5378dfe4deed23db1decc2574e26
NapolDeveloper <loma1016@gmail.com>
2025-01-13 13:09:04 +0900

    feat: 메타데이터 생성 코드
    - 레이아웃 패딩값 이슈도 함께 수정

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -1,10 +1,40 @@
+import { Metadata } from 'next';
+
 import { getPostDetail, getPostPaths, parsePostInfo, parseToc } from '@/lib/post';
+
 import PostBody from '../../_components/post/post-body';
 import PostHeader from '../../_components/post/post-header';
 import TableOfContent from '../../_components/post/table-of-content-sidebar';
+import { siteConfig } from '@/config/site';
 
 interface Props {
-  params: Promise<{ slug: string; category: string }>;
+  params: { slug: string; category: string };
+}
+
+export async function generateMetadata({ params: { category, slug } }: Props): Promise<Metadata> {
+  const post = await getPostDetail(category, slug);
+
+  const title = `${post.title} | NAPOL`;
+  const imageUrl = `${siteConfig.url}${post.thumbnail}`;
+
+  return {
+    title,
+    description: post.desc,
+
+    openGraph: {
+      title,
+      description: post.desc,
+      type: 'article',
+      publishedTime: post.date.toISOString(),
+      url: `${siteConfig.url}${post.url}`,
+      images: [imageUrl],
+    },
+    twitter: {
+      title,
+      description: post.desc,
+      images: [imageUrl],
+    },
+  };
 }
 
 export function generateStaticParams() {
@@ -26,7 +56,7 @@ export default async function PostDetailPage({ params }: Props) {
   const toc = parseToc(post.content);
 
   return (
-    <div className="prose mx-auto w-full max-w-[800px] px-0 dark:prose-invert">
+    <div className="prose mx-auto w-full max-w-[800px] dark:prose-invert">
       <PostHeader post={post} />
       <article className="relative">
         <PostBody post={post} />
--- src/config/site/index.ts
+++ src/config/site/index.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: '개발블로그',
+  name: 'NAPOL',
   url: 'http://localhost:3000',
   ogImage: '추후 추가 필요',
   description: '프론트 개발자의 개발 블로그',
