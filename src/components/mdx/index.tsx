2828e21238869692fd4f7df7815903a649dbaad1
NapolDeveloper <loma1016@gmail.com>
2025-01-14 15:35:36 +0900

    feat: del 커스텀 컴포넌트 구현

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,7 +1,9 @@
 import { MDXComponents } from 'mdx/types';
 
 import MdxImage from './MdxImage';
+import DeleteText from './DeleteText';
 
 export const MdxComponents: MDXComponents = {
   img: MdxImage,
+  del: DeleteText,
 };
