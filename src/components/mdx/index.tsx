08c241e74e4a7265cc1100aa4c5363c169df54b5
NapolDeveloper <loma1016@gmail.com>
2025-01-16 10:12:27 +0900

    feat: mdx 커스텀 테이블 구현

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -2,8 +2,12 @@ import { MDXComponents } from 'mdx/types';
 
 import MdxImage from './MdxImage';
 import DeleteText from './DeleteText';
+import { MdxTable, MdxTableHead, MdxTableCell } from './MdxTable';
 
 export const MdxComponents: MDXComponents = {
   img: MdxImage,
   del: DeleteText,
+  table: MdxTable,
+  th: MdxTableHead,
+  td: MdxTableCell,
 };
