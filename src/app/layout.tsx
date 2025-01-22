e73f0d5ca2baf82143edcd7fe917789683bfc826
NapolDeveloper <loma1016@gmail.com>
2025-01-22 13:04:08 +0900

    feat: 추가 되지 않은 아이콘 주석 처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,8 +76,8 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
+    // shortcut: '/favicon-16x16.png',
+    // apple: '/apple-touch-icon.png',
   },
   manifest: `${siteConfig.url}/site.webmanifest`,
 };
